f2ab74aee30ce5d155c9f2a2a9ab463513f4f04c
njiiriJohnii <njiirijohnie@yahoo.com>
2014-05-19 12:23:39 +0300

    bidding

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+
+	def index
+	end
+	def about
+    end
+    def news
+    end
+    def cows
+    end
+    def faq
+    end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dem::Application.config.secret_key_base = 'a42ab89dc0624c3fd84266fb6835e19063c2dbe87578007f291f26311bedde6eefc948403901a459fa8046dfdd07997ebdab6fdeef3951d9971e925c1ad6c562'
